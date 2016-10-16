a92ddacf6cbb93f55473a8f87559b92705e9e3cb
Ameer A. Mourad <ameeramourad@gmail.com>
2016-10-16 18:54:09 -0400

    Initial Commit

---
+++ Project Euler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Project Euler
+//
+//  Created by amourad on 10/16/16.
+//  Copyright © 2016 amourad. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
